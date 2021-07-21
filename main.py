c9cc71cb2d243e2261b0a2df34a31666029f353f
Pulkit Gopalani <gopalanipulkit1@gmail.com>
2021-07-21 21:29:00 +0530

    Add filters

--- main.py
+++ main.py
@@ -3,26 +3,43 @@ import matplotlib.pyplot as plt
 
 import pyaudio as pa
 
+
 def low_pass_filter(f_c, size):
-    freq_filter = np.concatenate(np.zeros((size-f_c,)), np.ones((2*f_c,)), np.zeros((size-f_c,)))
+    freq_filter = np.concatenate(
+        np.zeros((size - f_c,)), np.ones((2 * f_c,)), np.zeros((size - f_c,))
+    )
     return freq_filter
+
 
 def high_pass_filter(f_c, size):
-    freq_filter = np.concatenate(np.ones((size-f_c,)), np.zeros((2*f_c,)), np.ones((size-f_c,)))
+    freq_filter = np.concatenate(
+        np.ones((size - f_c,)), np.zeros((2 * f_c,)), np.ones((size - f_c,))
+    )
     return freq_filter
 
+
 def band_pass_filter(f_l, f_h, size):
-    freq_filter = np.concatenate(np.zeros((size-f_h,)), np.ones((f_h-f_l,)), np.zeros((2*f_l,)), np.ones((f_h-f_l,)), np.zeros((size-f_h,)))
+    freq_filter = np.concatenate(
+        np.zeros((size - f_h,)),
+        np.ones((f_h - f_l,)),
+        np.zeros((2 * f_l,)),
+        np.ones((f_h - f_l,)),
+        np.zeros((size - f_h,)),
+    )
     return freq_filter
+
 
 def lccde_filter(coeffs):
     pass
 
+
 def pole_zero_filter(poles, zeros):
     pass
 
+
 def hear_sound():
     pass
+
 
 def play_sound():
     pass
@@ -32,6 +49,6 @@ def process_real_time():
 
     in_stream = hear_sound()
 
-    #Process the sound here
+    # Process the sound here
 
     out_stream = play_sound()
