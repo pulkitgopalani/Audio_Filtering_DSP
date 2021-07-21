eaa00c74aea2ede2ea5a6862c01b3c481a2859af
Pulkit Gopalani <gopalanipulkit1@gmail.com>
2021-07-22 00:50:12 +0530

    Add docstrings

--- utils.py
+++ utils.py
@@ -7,6 +7,18 @@ CHANNELS = 1
 
 
 def hear_sound(record_time, sample_rate, chunk):
+    """
+    To record sound input in form of frames.
+
+    Inputs:
+        record_time (int): Time for which sound is recorded.
+        sample_rate (int): sampling rate parameter for processing
+        chunk (int): chunk parameter for processing
+
+    Outputs:
+        np_frames (np.ndarray): input frames as numpy array.
+    """
+
     p = pa.PyAudio()
 
     in_stream = p.open(
@@ -17,21 +29,39 @@ def hear_sound(record_time, sample_rate, chunk):
         input=True,
     )
 
-    frames = []
+    byte_frames = []
+    int_frames = []
 
     print("----Recording Audio----")
 
     for _ in range((record_time * sample_rate) / chunk):
         data_chunk = in_stream.read(chunk)
-        frames.append(data_chunk)
+        byte_frames.append(data_chunk)
+
+        for chunk in byte_frames:
+            int_frames.append(int(chunk))
+
+    np_frames = np.array(int_frames)
 
     in_stream.stop_stream()
     in_stream.close()
 
-    return frames
+    return np_frames
 
 
 def play_sound(audio, sample_rate, chunk):
+    """
+    To play given frames as audio.
+
+    Inputs:
+        audio (np.ndarray): numpy array of frames to play as audio.
+        sample_rate (int): sampling rate parameter for processing.
+        chunk (int): chunk parameter for processing
+
+    Outputs:
+        None
+    """
+
     p = pa.PyAudio()
 
     out_stream = p.open(
@@ -51,17 +81,44 @@ def play_sound(audio, sample_rate, chunk):
     out_stream.close()
     p.terminate()
 
-    return audio
-
 
 def plot_frames(frames):
+    """
+    Plot frames as matplotlib plot.
+
+    Inputs:
+        frames (np.ndarray): Frames (signal) to plot
+
+    Outputs:
+        None
+    """
     plt.plot(np.arange(frames.shape[-1], frames))
     plt.show()
 
 
 def preproc_and_fft_input(in_frames):
+    """
+    Preprocess the input signal and obtain its (zero-centered) FFT.
+
+    Inputs:
+        in_frames (np.ndarray): input signals in time domain in form of np.array
+
+    Outputs:
+        freq_output (np.ndarray): frequency domain (FFT) output for given input.
+    """
+
     pass
 
 
 def preproc_and_ifft_output(freq_output):
+    """
+    Preprocess the input signal and obtain its (zero-centered) FFT.
+
+    Inputs:
+        freq_output (np.ndarray): output signals in frequency domain in form of np.array
+
+    Outputs:
+        freq_output (np.ndarray): time domain (FFT) output for given input.
+    """
+
     pass
