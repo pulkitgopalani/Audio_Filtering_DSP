c417a5346fb8cf8695e159d5dcdb430a30a9dfca
Pulkit Gopalani <gopalanipulkit1@gmail.com>
2021-07-21 22:56:04 +0530

    Add Filter class

--- main.py
+++ main.py
@@ -1,78 +1,128 @@
-import os
 import argparse
 import numpy as np
 import pyaudio as pa
 import matplotlib.pyplot as plt
 
 FORMAT = pa.paInt16
-CHUNK = 1024
-REC_TIME = 5
 CHANNELS = 1
-SAMPLE_RATE = 44100
 
 root_dir = ""
 
 
-def hear_sound():
-    pass
+class Filter:
+    def __init__(self, filter_type, params):
+        self.filter_type = filter_type
+        self.params = params
+
+        self.filters = ["lowpass", "highpass", "bandpass", "lccde", "pz"]
+
+        try:
+            assert self.filter_type in self.filters
+
+            if self.filter_type == "lowpass":
+                self.filter = np.concatenate(
+                    np.zeros((self.params["size"] - self.params["f_c"],)),
+                    np.ones((2 * self.params["f_c"],)),
+                    np.zeros((self.params["size"] - self.params["f_c"],)),
+                )
+            elif self.filter_type == "highpass":
+                self.filter = np.concatenate(
+                    np.ones((self.params["size"] - self.params["f_c"],)),
+                    np.zeros((2 * self.params["f_c"],)),
+                    np.ones((self.params["size"] - self.params["f_c"],)),
+                )
+            elif self.filter_type == "bandpass":
+                self.filter = np.concatenate(
+                    np.zeros((self.params["size"] - self.params["f_h"],)),
+                    np.ones((self.params["f_h"] - self.params["f_l"],)),
+                    np.zeros((2 * self.params["f_l"],)),
+                    np.ones((self.params["f_h"] - self.params["f_l"],)),
+                    np.zeros((self.params["size"] - self.params["f_h"],)),
+                )
+            elif self.filter_type == "lccde":
+                self.filter = np.poly1d(self.params["coeffs"])
+            elif self.filter_type == "pz":
+                self.filter = np.poly1d(
+                    self.params["pz"]["zeros"], r=True
+                ) / np.poly1d(self.params["pz"]["poles"], r=True)
+
+        except AssertionError:
+            raise ValueError(
+                f"Invalid filter, please choose from {self.filters}"
+            )
+
+    def __call__(self, freq_domain_input):
+        try:
+            assert freq_domain_input.shape == self.filter.shape
+            print("----Applying filter in Frequency domain----")
+            output = self.filter * freq_domain_input
+            return output
+
+        except AssertionError:
+            raise ValueError("Shapes of filter and input do not match")
+
+    def get_filter(self):
+        return self.filter
 
 
-def play_sound():
-    pass
-
-
-def low_pass_filter(f_c, size):
-    filter = np.concatenate(
-        np.zeros((size - f_c,)), np.ones((2 * f_c,)), np.zeros((size - f_c,))
+def hear_sound(record_time, sample_rate, chunk):
+    p = pa.PyAudio()
+    in_stream = p.open(
+        format=FORMAT,
+        rate=sample_rate,
+        channels=CHANNELS,
+        frames_per_buffer=chunk,
+        input=True,
     )
-    print("Filter used: {}".format(filter))
 
-    return filter
+    frames = []
+
+    print("----Recording Audio----")
+
+    for _ in range((record_time * sample_rate) / chunk):
+        data_chunk = in_stream.reaAssertionErrord(chunk)
+        frames.append(data_chunk)
+
+    in_stream.stop_stream()
+    in_stream.close()
+
+    return frames
 
 
-def high_pass_filter(f_c, size):
-    filter = np.concatenate(
-        np.ones((size - f_c,)), np.zeros((2 * f_c,)), np.ones((size - f_c,))
+def play_sound(audio, sample_rate, chunk):
+    p = pa.PyAudio()
+    out_stream = p.open(
+        format=FORMAT,
+        rate=sample_rate,
+        channels=CHANNELS,
+        frames_per_buffer=chunk,
+        output=True,
     )
-    print("Filter used: {}".format(filter))
 
-    return filter
+    print("----Playing Audio----")
+
+    for data_chunk in audio:
+        out_stream.write(data_chunk)
+
+    out_stream.stop_stream()
+    out_stream.close()
+    p.terminate()
+
+    return audio
 
 
-def band_pass_filter(f_l, f_h, size):
-    filter = np.concatenate(
-        np.zeros((size - f_h,)),
-        np.ones((f_h - f_l,)),
-        np.zeros((2 * f_l,)),
-        np.ones((f_h - f_l,)),
-        np.zeros((size - f_h,)),
+def process_real_time(**kwargs):
+
+    in_data = hear_sound(
+        kwargs["time"], kwargs["sample_rate"], kwargs["chunk"]
     )
-    print("Filter used: {}".format(filter))
-
-    return filter
-
-
-def lccde_filter(coeffs):
-    filter = np.poly1d(coeffs)
-    print("Filter used: {}".format(filter))
-
-    return filter
-
-
-def pole_zero_filter(poles, zeros):
-    filter = np.poly1d(zeros, r=True) / np.poly1d(poles, r=True)
-    print("Filter used: {}".format(filter))
-
-    return filter
-
-
-def process_real_time():
-
-    in_stream = hear_sound()
 
     # Process the sound here
+    filter = Filter(kwargs["filter_type"], kwargs["size"], kwargs["params"])
 
-    out_stream = play_sound()
+    out_data = play_sound(
+        kwargs["time"], kwargs["sample_rate"], kwargs["chunk"]
+    )
 
 
 def main(args):
@@ -90,14 +140,31 @@ if __name__ == "__main__":
         default="low_pass",
         help="Type of filter, options: low_pass, high_pass, band_pass, pole_zero, lccde",
     )
+
     parser.add_argument(
         "--time", type=int, default=5, help="Time to record, default: 5 sec"
     )
+
+    parser.add_argument(
+        "--chunk",
+        type=int,
+        default=1024,
+        help="Chunk parameter, default: 1024",
+    )
+
+    parser.add_argument(
+        "--sample_rate",
+        type=int,
+        default=44100,
+        help="Sampling rate, default: 44100",
+    )
+
     parser.add_argument(
         "--pre-recorded",
         action="store_true",
         help="To use prerecorded sound as test",
     )
+
     parser.add_argument(
         "--pre-recorded-audio",
         type="str",
