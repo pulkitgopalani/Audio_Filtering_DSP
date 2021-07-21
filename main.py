6dfef9459f7313c163202d6e87113864b6875727
Pulkit Gopalani <gopalanipulkit1@gmail.com>
2021-07-21 21:28:15 +0530

    Add filters

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import matplotlib.pyplot as plt
 
 import pyaudio as pa
 
-def low_pass_filter(f_c):
-    pass
+def low_pass_filter(f_c, size):
+    freq_filter = np.concatenate(np.zeros((size-f_c,)), np.ones((2*f_c,)), np.zeros((size-f_c,)))
+    return freq_filter
 
-def high_pass_filter(f_c):
-    pass
+def high_pass_filter(f_c, size):
+    freq_filter = np.concatenate(np.ones((size-f_c,)), np.zeros((2*f_c,)), np.ones((size-f_c,)))
+    return freq_filter
 
-def band_pass_filter(f_l, f_h):
-    pass
+def band_pass_filter(f_l, f_h, size):
+    freq_filter = np.concatenate(np.zeros((size-f_h,)), np.ones((f_h-f_l,)), np.zeros((2*f_l,)), np.ones((f_h-f_l,)), np.zeros((size-f_h,)))
+    return freq_filter
 
 def lccde_filter(coeffs):
     pass
