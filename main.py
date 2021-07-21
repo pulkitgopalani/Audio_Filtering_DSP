37beb214832f279f9144e9b0ca013303c96e2dd5
Pulkit Gopalani <gopalanipulkit1@gmail.com>
2021-07-21 21:20:17 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import pyaudio as pa
+
+def low_pass_filter(f_c):
+    pass
+
+def high_pass_filter(f_c):
+    pass
+
+def band_pass_filter(f_l, f_h):
+    pass
+
+def lccde_filter(coeffs):
+    pass
+
+def pole_zero_filter(poles, zeros):
+    pass
+
+def hear_sound():
+    pass
+
+def play_sound():
+    pass
+
+
+def process_real_time():
+
+    in_stream = hear_sound()
+
+    #Process the sound here
+
+    out_stream = play_sound()
